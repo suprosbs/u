ca5673cb7f566e9f139e82c79a5456d853da0119
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2025-02-03 17:00:21 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,19 +3,19 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","6754465323:AAFBJEW3cCuni3WZ_6oerxGRlI4yCFrYZl4")
 
     SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
 
-    API_ID = int(os.environ.get("API_ID"))
+    API_ID = int(os.environ.get("API_ID","2944318"))
 
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("API_HASH","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")
 
-    CLIENT_ID = os.environ.get("CLIENT_ID")
+    CLIENT_ID = os.environ.get("CLIENT_ID","69689902615-3b0c0tgg7me2pulu9vftvnsf9o9mpf6i.apps.googleusercontent.com")
 
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+    CLIENT_SECRET = os.environ.get("CLIENT_SECRET","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")
 
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER",6865672542"))
 
     AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
 
